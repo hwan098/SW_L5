226efb0ed25bae7edb393103407e29ee59655032
hwan098 <glqkfl7845@gmail.com>
2023-01-27 11:59:06 +0900

    2.제품코드 결과화면 생성 rusult.jsp 및 빈즈 파일 생성 후 코드 조회

---
+++ work/src/main/java/work/UserInfo.java
@@ -0,0 +1,32 @@
+package work;
+
+public class UserInfo {
+	private String name;
+	private String code;
+	private String date;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public UserInfo(String name, String code, String date) {
+		super();
+		this.name = name;
+		this.code = code;
+		this.date = date;
+	}
+
+}
